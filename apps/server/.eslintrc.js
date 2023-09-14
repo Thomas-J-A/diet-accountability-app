001280bcdb3fb9a8e6574ddbb4175780029c83c0
Thomas Aspbury <tomaspbury@icloud.com>
2023-09-14 14:05:00 +0100

    chore(eslint): update eslint config

---
+++ apps/server/.eslintrc.js
@@ -0,0 +1,43 @@
+export default {
+  root: true,
+  ignorePatterns: [
+    'node_modules',
+    'build',
+    'package-lock.json',
+    '.eslintrc.js',
+  ],
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      processor: '@graphql-eslint/graphql',
+    },
+    {
+      files: ['*.graphql'],
+      extends: 'plugin:@graphql-eslint/schema-recommended',
+      parserOptions: {
+        schema: './src/schema.graphql',
+      },
+    },
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [2, { argsIgnorePattern: '^_' }],
+  },
+};
