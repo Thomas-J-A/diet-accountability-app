3f7ccc9917cd908505b575c5be7bee0d1b03e45b
Thomas Aspbury <tomaspbury@icloud.com>
2023-09-17 11:52:23 +0100

    chore(graphql): fix graphql tooling
    Created a standard GraphQL config file in root of project which both IDE extensions and npm libs
    (@graphql-eslint/eslint-plugin) can use

--- apps/server/.eslintrc.js
+++ apps/server/.eslintrc.js
@@ -1,43 +1,47 @@
+// Top level TypeScript config inside override so that
+// TypeScript and GraphQL parsers don't do battle
 module.exports = {
   root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
   ignorePatterns: [
     'node_modules',
     'build',
     'package-lock.json',
     '.eslintrc.js',
   ],
-  env: {
-    node: true,
-    es6: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/strict-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-  },
-  plugins: ['@typescript-eslint'],
   overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'prettier',
+      ],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [2, { argsIgnorePattern: '^_' }],
+      },
+    },
     {
       files: ['*.ts'],
       processor: '@graphql-eslint/graphql',
     },
     {
       files: ['*.graphql'],
-      extends: 'plugin:@graphql-eslint/schema-recommended',
-      parserOptions: {
-        schema: './src/schema.graphql',
-      },
+      extends: ['plugin:@graphql-eslint/schema-recommended'],
+      // parser: '@graphql-eslint/eslint-plugin',
+      // plugins: ['@graphql-eslint'],
     },
   ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': [2, { argsIgnorePattern: '^_' }],
-  },
 };
