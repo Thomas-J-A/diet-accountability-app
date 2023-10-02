a5d90e3edaf7215b15edf2411cddf36c016bbe40
Thomas-J-A <70804406+Thomas-J-A@users.noreply.github.com>
2023-10-02 08:02:40 +0100

    feat(api): add update meal functionality (#9)

--- apps/server/src/services/meal.service.ts
+++ apps/server/src/services/meal.service.ts
@@ -3,10 +3,25 @@ import { HydratedDocument } from 'mongoose';
 import {
   CreateMealInput,
   CreateMealPayload,
+  UpdateMealInput,
+  UpdateMealPayload,
 } from '../__generated__/resolvers-types';
 import Meal, { IMeal } from '../models/meal.model';
 import DayEvent, { IDayEvent } from '../models/day-event.model';
 import ErrorCodes from '../types/error-codes';
+import isEmptyObject from '../utils/is-empty-object';
+import isValidObjectId from '../utils/is-valid-object-id';
+
+// Reusable function for ensuring /rating/ value is within range
+const validateRating = (rating: number) => {
+  if (!(rating >= 1 && rating <= 10)) {
+    throw new GraphQLError('Rating must be between 1 and 10 inclusive', {
+      extensions: {
+        code: ErrorCodes.BAD_USER_INPUT,
+      },
+    });
+  }
+};
 
 const createMeal = async (input: CreateMealInput, userId: string) => {
   const { date, description, location, rating, type } = input;
@@ -61,4 +76,70 @@ const createMeal = async (input: CreateMealInput, userId: string) => {
   return response;
 };
 
-export default { createMeal };
+// Represents possible updates to meal document
+interface UpdatedFields {
+  description?: string;
+  location?: string;
+  rating?: number;
+}
+
+const updateMeal = async (input: UpdateMealInput) => {
+  const { id, description, location, rating } = input;
+
+  // Ensure id is a valid ObjectId
+  if (!isValidObjectId(id)) {
+    throw new GraphQLError(
+      'You must provide a valid ObjectId for the id field',
+      {
+        extensions: {
+          code: ErrorCodes.BAD_USER_INPUT,
+        },
+      },
+    );
+  }
+
+  // Locate only the updated fields
+  const updatedFields: UpdatedFields = {};
+
+  if (description) updatedFields.description = description;
+  if (location) updatedFields.location = location;
+  if (rating) updatedFields.rating = rating;
+
+  // Verify that at least one value has been changed
+  if (isEmptyObject(updatedFields)) {
+    throw new GraphQLError('You must specify at least one updated value', {
+      extensions: {
+        code: ErrorCodes.BAD_USER_INPUT,
+      },
+    });
+  }
+
+  // Ensure rating is within range, if supplied
+  if (updatedFields.rating) validateRating(updatedFields.rating);
+
+  // Update record in database
+  const updatedMeal = await Meal.findByIdAndUpdate(id, updatedFields, {
+    new: true,
+  }).exec();
+
+  // Ensure a meal document was even found
+  if (!updatedMeal) {
+    throw new GraphQLError(`A meal with id of ${id} was not found`, {
+      extensions: {
+        code: ErrorCodes.BAD_USER_INPUT,
+      },
+    });
+  }
+
+  // Format and return response
+  const response: UpdateMealPayload = {
+    code: '200',
+    success: true,
+    message: 'Meal was successfully updated',
+    meal: updatedMeal,
+  };
+
+  return response;
+};
+
+export default { createMeal, updateMeal };
