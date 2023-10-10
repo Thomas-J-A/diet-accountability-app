117faf9cc487f0578995d4eb0fecc723321b3e6d
Thomas Aspbury <tomaspbury@icloud.com>
2023-10-10 09:33:45 +0100

    refactor: create a reusable createApolloServer function
    Express app and tests can now use the same createApolloServer function to initialize an Apollo
    Server instance

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,7 +1,36 @@
-import app from './app';
+import express, { Application } from 'express';
+import cors from 'cors';
+import { expressMiddleware } from '@apollo/server/express4';
+import connectDB from './configs/db';
 import env from './configs/env';
+import { createContext } from './auth/context';
+import createApolloServer from './utils/create-apollo-server';
 
 const PORT = env.PORT;
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+// Function body contains a catch block for errors
+void connectDB();
+
+// Create and start up Apollo server instance
+const startServer = async () => {
+  const server = createApolloServer();
+
+  await server.start();
+
+  // Add Apollo Server middleware to app which handles graphql requests
+  app.use('/graphql', expressMiddleware(server, { context: createContext }));
+};
+
+startServer().catch(() => {
+  console.error('Error starting Apollo server instance');
+  process.exit(1);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
