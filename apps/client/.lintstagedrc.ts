27398002abaf023d2a748aad7adf17a3ba10b785
Thomas-J-A <70804406+Thomas-J-A@users.noreply.github.com>
2023-10-21 09:47:40 +0100

    chore(config): update lint-staged config globs (#18)

--- apps/client/.lintstagedrc.ts
+++ apps/client/.lintstagedrc.ts
@@ -1,13 +1,13 @@
 // TODO: stylelint --fix
 
 const config = {
-  '*.ts?(x)': [
+  '**/*.ts?(x)': [
     () => 'tsc -p ./apps/client/tsconfig.json --noEmit --skipLibCheck',
     'eslint --fix',
     'prettier --write',
     // 'jest --config ./apps/client/jest.config.ts --bail --findRelatedTests --passWithNoTests',
   ],
-  '*': 'gitleaks protect -v --staged',
+  '**/*': 'gitleaks protect -v --staged',
 };
 
 export default config;
