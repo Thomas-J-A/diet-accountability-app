179fc81c11ad1b94d1b20790e0388e0fa02d5eb5
Thomas Aspbury <tomaspbury@icloud.com>
2023-10-21 09:41:50 +0100

    chore(config): update lint-staged config globs

--- apps/server/.lintstagedrc.js
+++ apps/server/.lintstagedrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  '*.ts': [
+  '**/*.ts': [
     () => 'tsc -p ./apps/server/tsconfig.json --noEmit --skipLibCheck',
     'eslint --fix',
     'prettier --write',
     'jest --config ./apps/server/jest.config.ts --bail --findRelatedTests --passWithNoTests',
   ],
-  '*': 'gitleaks protect -v --staged',
+  '**/*': 'gitleaks protect -v --staged',
 };
