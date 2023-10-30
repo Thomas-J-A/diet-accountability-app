7bd4c8b1738b7cd48fd1708031654cb138a06f94
Thomas Aspbury <tomaspbury@icloud.com>
2023-10-30 08:36:27 +0000

    refactor: remove empty style declarations

--- apps/client/src/components/layout/Drawer/DrawerBody/DrawerBody.styled.tsx
+++
@@ -1,4 +0,0 @@
-import styled from 'styled-components';
-import { Flex } from '@radix-ui/themes';
-
-export const DrawerBody = styled(Flex)``;
--- apps/client/src/components/layout/Drawer/DrawerBody/DrawerBody.tsx
+++ apps/client/src/components/layout/Drawer/DrawerBody/DrawerBody.tsx
@@ -1,29 +1,28 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { Heading, Text, VisuallyHidden } from '@radix-ui/themes';
+import { Flex, Heading, Text, VisuallyHidden } from '@radix-ui/themes';
 import Menu from '../../Menu/Menu';
-import * as S from './DrawerBody.styled';
 
 const DrawerBody = ({ isAuthed }: { isAuthed: boolean }) => {
   if (isAuthed) {
     return (
-      <S.DrawerBody direction="column" gap="2" grow="1">
+      <Flex direction="column" gap="2" grow="1">
         <VisuallyHidden asChild>
           <Dialog.Title asChild>
             <Heading size="3">Menu</Heading>
           </Dialog.Title>
         </VisuallyHidden>
         <Menu />
-      </S.DrawerBody>
+      </Flex>
     );
   }
 
   return (
-    <S.DrawerBody direction="column" gap="2">
+    <Flex direction="column" gap="2">
       <Dialog.Title asChild>
         <Heading size="3">Sign in using your email and password</Heading>
       </Dialog.Title>
       <Text>Sign In fields here</Text>
-    </S.DrawerBody>
+    </Flex>
   );
 };
 
