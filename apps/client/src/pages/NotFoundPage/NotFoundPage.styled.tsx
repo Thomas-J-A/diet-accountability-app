87eb570fec2b5102c739c60d1c6f7ea72a2f3adc
Thomas Aspbury <tomaspbury@icloud.com>
2023-11-10 07:55:07 +0000

    feat(logo): change logo into contextual link

--- apps/client/src/pages/NotFoundPage/NotFoundPage.styled.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-
-export const StyledLink = styled(Link)`
-  color: var(--accent-a11);
-  text-decoration: none;
-`;
