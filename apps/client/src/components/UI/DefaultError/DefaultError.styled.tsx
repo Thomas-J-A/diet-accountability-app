78c631f811fd88897aa117f075a222e6ac526048
Thomas Aspbury <tomaspbury@icloud.com>
2023-11-11 08:06:38 +0000

    style: update UI of default error boundary to fit theme

---
+++ apps/client/src/components/UI/DefaultError/DefaultError.styled.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { Flex } from '@radix-ui/themes';
+
+export const DefaultError = styled(Flex)`
+  min-height: 100vh;
+`;
--- apps/client/src/components/UI/DefaultError/DefaultError.tsx
+++ apps/client/src/components/UI/DefaultError/DefaultError.tsx
@@ -1,5 +1,25 @@
+import { Flex, Heading, Text } from '@radix-ui/themes';
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
+import * as S from './DefaultError.styled';
+
 const DefaultError = () => {
-  return <div>Big fat error</div>;
+  return (
+    <S.DefaultError
+      direction="column"
+      justify="center"
+      align="center"
+      gap="5"
+      p="3"
+    >
+      <Flex direction="column" align="center" gap="2">
+        <ExclamationTriangleIcon height="70" width="70" />
+        <Heading size={{ initial: '6', md: '8' }}>
+          Oops! Something went south...
+        </Heading>
+        <Text>Try refreshing the page or visit again later.</Text>
+      </Flex>
+    </S.DefaultError>
+  );
 };
 
 export default DefaultError;
