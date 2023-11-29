4592feb1d9b25560b799b259612ca0756fb998a4
Thomas-J-A <70804406+Thomas-J-A@users.noreply.github.com>
2023-11-29 12:04:53 +0000

    feat(raqs): implement RAQs page (#47)

--- apps/client/src/pages/RAQsPage/RAQsPage.tsx
+++ apps/client/src/pages/RAQsPage/RAQsPage.tsx
@@ -1,12 +1,40 @@
-import { Box, Text } from '@radix-ui/themes';
-import * as S from './RAQsPage.styled';
+import {
+  Container,
+  Grid,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Em,
+  Separator,
+} from '@radix-ui/themes';
+import QuestionsAccordion from './QuestionsAccordion/QuestionsAccordion';
 
 const RAQsPage = () => {
   return (
-    <Box p="3" asChild>
-      <S.RAQsPage>
-        <Text>RAQs Stuff</Text>
-      </S.RAQsPage>
+    <Box p={{ initial: '3', md: '6' }} asChild>
+      <main>
+        <Container size={{ initial: '1', md: '3' }}>
+          <Grid gap="3" columns={{ md: '5fr 4fr' }}>
+            <Flex direction="column" gap={{ initial: '2', md: '3' }}>
+              <Heading size={{ initial: '7', md: '8' }}>
+                Rarely Asked Questions
+              </Heading>
+              <Text size={{ initial: '2', md: '3' }}>
+                Welcome to our RAQs section. Nobody asked,{' '}
+                <Em>but here are the answers anyway.</Em>
+              </Text>
+              <Separator size="3" />
+            </Flex>
+
+            <Box height="9" style={{ background: 'red' }}>
+              Ads
+            </Box>
+
+            <QuestionsAccordion />
+          </Grid>
+        </Container>
+      </main>
     </Box>
   );
 };
