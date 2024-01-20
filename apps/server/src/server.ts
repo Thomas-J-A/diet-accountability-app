0690dd3aad22c4fe6a0c74ace376b0b815ca3c48
Thomas Aspbury <tomaspbury@icloud.com>
2024-01-20 18:10:29 +0000

    chore: prepare app for deployment

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import express, { Application } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -13,9 +14,11 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
-// CSP settings too restrictive for dev and testing
-// (blocking Apollo Server Playground, etc)
+// Serve static client files in production
+// Also, only allow helmet middleware in prod
+// CSP settings too restrictive for dev and testing (blocking Apollo Server Playground, etc)
 if (env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'dist')));
   app.use(helmet());
 }
 
@@ -37,6 +40,20 @@ startServer().catch(() => {
   console.error('Error starting Apollo server instance');
   process.exit(1);
 });
+
+// HTTP health check endpoint so hosting platform can check health of app
+app.get('/health', (_, res) => {
+  res.send('ok');
+});
+
+// Catch-all route for non static/non GraphQL routes, intended to be
+// handled by react-router on client (client/page, /users/12, /foo, etc)
+// index.html will be returned and react-router will do client-side routing to page
+if (env.NODE_ENV === 'production') {
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
