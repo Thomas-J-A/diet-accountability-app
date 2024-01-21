1296d641eb36c721bb57fa190e2997a1465ebcfd
Thomas-J-A <70804406+Thomas-J-A@users.noreply.github.com>
2024-01-21 17:22:00 +0000

    feat(header): make main header sticky on scroll (#81)

--- apps/client/src/components/layout/MainHeader/MainHeader.styled.tsx
+++ apps/client/src/components/layout/MainHeader/MainHeader.styled.tsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
 export const MainHeader = styled.header`
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  background-color: var(--gray-1);
   border-bottom: 1px solid var(--gray-a6);
 `;
